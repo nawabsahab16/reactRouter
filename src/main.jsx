1a402bb76946e28a311528dcdafce702d1820606
nawabsahab16 <nawab162004@gmail.com>
2023-10-15 19:06:24 +0530

    feat: implemented RouterProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
 
 const router = createBrowserRouter([
   {
-    path : '/'
+    path : '/',
+    element: <Layout />,
+    children : [
+      {
+       path : "",
+       element : <Home />
+      },
+      {
+        path : "about",
+        element : <About />
+      }
+
+]
   }
 ])
 
